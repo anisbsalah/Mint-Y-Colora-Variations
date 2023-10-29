e8244162787fc8fe02ec7753996e9dd848bba65e
anisbsalah <anisbensalah.89@gmail.com>
2023-10-29 16:33:40 +0100

    Update v1-up.sh

--- v1-up.sh
+++ v1-up.sh
@@ -41,6 +41,39 @@
 # $ git push --force-with-lease origin main
 # $ git push --force origin main
 
+# Undo Local Changes That Have Not Been Committed
+# To see the affected file listed run:
+# $ git status
+# To revert a file to the way it was at the previous commit (before your changes) run:
+# $ git checkout <file_path>
+
+# Undo a Specific Commit (That Has Been Pushed)
+# $ git status # to make sure you have a clean working tree
+# To find the hash for the commit you want to undo run:
+# $ git log
+# Or:
+# $ git log --oneline
+# Once you know the hash for the commit you want to undo, run the following command ( The --no-edit option prevents git from asking you to enter in a commit message. If you don't add that option, you'll end up in the text editor ):
+# $ git revert <SHA> --no-edit
+# If working with a remote repo, you can now push those changes:
+# $ git push
+
+# Undoing Your Last Commit (That Has Not Been Pushed)
+# $ git reset --soft HEAD~
+# The same as:
+# $ git reset --soft HEAD^
+# Add a number to the end to undo multiple commits
+# For example, to undo the last 2 commits (assuming both have not been pushed) run:
+# $ git reset --soft HEAD~2
+
+# Undoing Local Changes That Have Been Committed (But Not Pushed)
+# To make sure you have a clean working tree run:
+# $ git status
+# To remove the commits, but the changes will appear as uncommitted run:
+# $ git reset <SHA>
+# To undo the commits and through away the code run:
+# $ git reset --hard <SHA>
+
 ##################################################################################################################
 
 # githubdir="$(basename "$(dirname "${PWD}" | tr '[:upper:]' '[:lower:]')")"
